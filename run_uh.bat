429cc903d23e2d1e00dfcde7bc1a5fe09fe49370
James <49895469+j-richey@users.noreply.github.com>
2019-04-27 14:56:56 -0700

    Adding quotes around paths that may contain spaces
    Adding quotes allows the bat file to work with paths that contain
    spaces. See issue #2907.

--- run_uh.bat
+++ run_uh.bat
@@ -5,19 +5,19 @@ if '%1' == '--debug' goto debugMode
 if '%1' == '--help' goto help
 
 echo output will be written to logfile "unknownhorizons-DATETIME.log"
-IF EXIST %~dp0..\python\python.exe (
-  %~dp0..\python\python.exe %~dp0run_uh.py --debug-log-only
+IF EXIST "%~dp0..\python\python.exe" (
+  "%~dp0..\python\python.exe" "%~dp0run_uh.py" --debug-log-only
 ) ELSE (
-  %~dp0run_uh.py --debug-log-only
+  "%~dp0run_uh.py" --debug-log-only
 )
 pause
 goto:EOF
 
 :debugMode
-IF EXIST %~dp0..\python\python_d.exe (
-  %~dp0..\python\python_d.exe run_uh.py --debug-log-only
+IF EXIST "%~dp0..\python\python_d.exe" (
+  "%~dp0..\python\python_d.exe" run_uh.py --debug-log-only
 ) ELSE (
-  %~dp0run_uh.py --debug-log-only
+  "%~dp0run_uh.py" --debug-log-only
 )
 if errorlevel 1 (echo.
 echo Error! Please check if you have Python Debug installed!
